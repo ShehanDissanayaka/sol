fd62a9bc10eb5c959c0e2cd98200bda776c19391
ShehanDissanayaka <chamodshehan2019@gmail.com>
2025-04-07 22:40:35 +0530

    Fix: unoptimized image support for Netlify

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-  };
-  
-  export default nextConfig;
-  
+    images: {
+        unoptimized: true,
+    }
+};
+
+export default nextConfig;
